9366cd2f956cd792f5deec0f1ac7310fa43d33a4
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-10-08 16:27:44 +0200

    Undid code errors made this morning, code goes as far as updating the array with X and 0 for the FIRST player1choic and FIRST player2 choise only, so far

--- script.js
+++ script.js
@@ -30,90 +30,75 @@ function createPlayerObject (player) {
 
 
 
+let gameboard = function (){
+    //empty array elements upon which Xs or 0s can be generated
+  return new Array(9);
+  
+
+} ();
 
 
+for (let i= 0; i < gameboard.length ; i++)
+    {//assigning values equivalent to index to each array position
+        //equate sqaures to 1 to 9 rather than 0 to 8, to make the board easier to visualise
+        gameboard[i]=i+1;
+    
+       
+    }   
+
+
+
+function playRound () {
 const player1 = prompt('Enter the name of player 1');
 createPlayerObject (player1);
 
 //need to figure out how to associate name grabbed here with createPlayerObject
 const player2 = prompt('Enter the name of player 2');
 createPlayerObject (player2);
-let player1Choice = prompt('Player 1, enter a value between 0 and 8');
-let player2Choice= prompt('Player 2, enter a value between 0 and 8');
-player1Choice*=1;
-player2Choice*=1;
-
+let player1Choice = prompt('Player 1, enter a value between 1 and 9');
+if (player1Choice<1 || player1Choice>9)
+{player1Choice = prompt('You may only enter a value between 1 and 9');}
 console.log(player1Choice);
 
 
-let gameboard = function (){
-    //empty array elements upon which Xs or 0s can be generated
-  return new Array(9);
-  
-    
-    
-} ();
 
-
-
-
-for (let i= 0; i < gameboard.length; i++)
-    {//assigning values equivalent to index to each array position
-        gameboard[i]=i;
-       
-        // console.log(player1Choice);
-    }    
-
-
-console.log(player1Choice);
-
-if (gameboard.includes(player1Choice))
-
-
-    {gameboard[player1Choice]='X';
-        console.log(gameboard[player1Choice]);
-        console.log('first if stat');
-        console.log(gameboard);
-       
-    }
-
-   if (gameboard.includes(player2Choice))
-
-
-        {gameboard[player2Choice]='0';
-        console.log('second if stat');
-            console.log(gameboard);
-        }
-
-
-
-
-
-    
-
-
-
-
-
-console.log(player1);
-console.log(player2);
-console.log(player1Choice);
+let player2Choice= prompt('Player 2, enter a value between 1 and 9');
+if (player2Choice<1 || player2Choice>9)
+    {player2Choice = prompt('You may only enter a value between 1 and 9');}
 console.log(player2Choice);
 
 
 
+player1Choice*=1;
+player2Choice*=1;
+if (gameboard.includes(player1Choice)) {
+    gameboard[player1Choice]='X';
+    
+   // player1Choice = prompt ('This number has already been selected, choose a different one');
+
+}
+
+if (gameboard.includes(player2Choice)) {
+    gameboard[player2Choice]='0';
+
+    // player2Choice = prompt ('This number has already been selected, choose a different one');
+
+} 
+console.log(gameboard);
+}
 
 
+playRound();
 
+// switch (a,b,c)
+// {case: a===b; 
+// console.log('it works');
+// break;
+// case: b===c;
+// console.log('it still works');
+// break;
 
-
-
-
-
-
-
-
-
+// }
 
 }
 
---
+++ script1.js
@@ -0,0 +1,14 @@
+
+
+let array = [1,1,3,4,5,6,7,8,9];
+
+switch ([array])
+{case array[0].value===1: 
+console.log('the same');
+break;
+case array[0]!==array[1]:
+console.log('not the same');
+break;
+
+}
+
