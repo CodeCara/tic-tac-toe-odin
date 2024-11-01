2175c4565938aa070452d432f12d9bdc8d1cc57b
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-11-01 18:19:58 +0100

    Fixed bug which allowed game to be played without player names entered, which caused other bugs

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ let gameboard = function (){
     " ", " ", " "]
     
     let newboard = [...board];
-    // let currentPlayer = 'Player One';
 let currentPlayer;
 
     let squareClicked;
     
-    const getBoard = () => board; 
     
     console.log(board);
-    //Board is being reset correctly (tested)
     let resetBoardAndGame = function (){
         board = [...newboard];
         gameboard.board=board;
@@ -39,18 +36,16 @@ let currentPlayer;
         return board;
     };
 
+
     let gameboardFull = function (board) {
-        //how to shorten the if statement?
      if (gameboard.board.includes(' '))
     
         {return false;}
      else {return true;}
-
-
     };
 
 
-  return {board, gameboardFull, resetBoardAndGame, getBoard, squareClicked, currentPlayer};
+  return {board, gameboardFull, resetBoardAndGame, squareClicked, currentPlayer};
   
 
 } ();
@@ -63,31 +58,14 @@ let createPlayerObject = function(playerName) {
     const name = playerName; 
   //Should value below be left as null?
    const currentPlayerChoice = null;
-        const playerWins=false;
-       console.log(playerName);
-        return {name, currentPlayerChoice, playerWins}
+        return {name, currentPlayerChoice}
     }
-
-
-
-
-
-
-
 
 
 let playGame = function () {
     let gameOver = false;
-    let winner;
    
-
-
-// const firstPlayerObject = createPlayerObject('Player One');
-// const secondPlayerObject = createPlayerObject('Player Two');
-
-
-
-
+    //Logic for Player One win
     if ((gameboard.board[0]==='X' && gameboard.board[1]==='X' && gameboard.board[2]==='X') ||
     (gameboard.board[3]==='X' && gameboard.board[4]==='X' && gameboard.board[5]==='X') ||
     (gameboard.board[6]==='X' && gameboard.board[7]==='X' && gameboard.board[8]==='X') ||   
@@ -97,21 +75,14 @@ let playGame = function () {
     (gameboard.board[0]==='X' && gameboard.board[4]==='X' && gameboard.board[8]==='X') ||
     (gameboard.board[6]==='X' && gameboard.board[4]==='X' && gameboard.board[2]==='X'))
     {
-        console.log(gameboard.gameboardFull());
         displayController.messageBox.innerText=`${displayController.firstPlayerObject.name} wins!  Click anywhere to continue`;
         displayController.messageBox.style.display='flex';
         displayController.firstPlayerObject.playerWins = true;
-        winner='Player One';
-        console.log(gameboard.board);
-
-        // console.log(firstPlayerObject);
-        // console.log(secondPlayerObject);
-        // console.log(firstPlayerObject.currentPlayerChoice);
         gameOver=true;
         
     }
 
-
+    //Logic for Player Two win
     else if ((gameboard.board[0]==='0' && gameboard.board[1]==='0'&& gameboard.board[2]==='0') ||
     (gameboard.board[3]==='0' && gameboard.board[4]==='0' && gameboard.board[5]==='0') ||
     (gameboard.board[6]==='0' && gameboard.board[7]==='0' && gameboard.board[8]==='0') ||   
@@ -121,42 +92,25 @@ let playGame = function () {
     (gameboard.board[0]==='0' && gameboard.board[4]==='0' && gameboard.board[8]==='0') ||
     (gameboard.board[6]==='0' && gameboard.board[4]==='0' && gameboard.board[2]==='0'))
     {
-        console.log(gameboard.gameboardFull());
 
         displayController.messageBox.innerText=`${displayController.secondPlayerObject.name} wins!  Click anywhere to continue`;
         displayController.messageBox.style.display='flex';
-
         displayController.secondPlayerObject.playerWins = true;
-        winner='Player Two';
-        console.log(gameboard.board);
-        // console.log(firstPlayerObject.currentPlayerChoice);
         gameOver=true;
         
     }
 
-    console.log(gameOver);
-    console.log(gameboard.gameboardFull());
-
-
+    //Logic for no-win situation
     if (gameboard.gameboardFull()===true && gameOver===false) 
 
 {
     displayController.messageBox.innerText='No winner! Click anywhere to continue' ;
-
     displayController.messageBox.style.display='flex';
-
-
     gameOver=true;
-    console.log(gameboard.board);
-    console.log(displayController.messageBox.parentNode);
-
-    console.log(gameOver);
 }
 
 
-return {winner};
 }
-
 
 
 
@@ -194,36 +148,26 @@ let displayController = function(){
     }
 
     const captureNameInputs = function(){
-        console.log(displayController.playerOneName.value);;
-        console.log(playerTwoName.value);
+        if (playerOneName.value==='' || playerTwoName.value === '')
+            {alert("You must enter both names and press the 'Play' button to start")}
+
+    else {
         boardPlusReset.style.display='flex';
-        // playerNamesContainer.style.display='none';
         firstPlayerObject.name=playerOneName.value;
         secondPlayerObject.name = playerTwoName.value;
-        console.log(firstPlayerObject);
-        console.log(displayController.playerOneName.value);
-        console.log({displayController});
         gameboard.currentPlayer=firstPlayerObject.name;
         playerOneName.style.backgroundColor='rgb(245, 245, 133)';
         playerTwoName.style.backgroundColor='rgb(245, 245, 133)';
-        // namesText.style.display='none';
-
-
- console.log(firstPlayerObject);
-
+    }
     }
 
     playButton.addEventListener('click', captureNameInputs)
 
     messageBox.addEventListener('click', closeMessageBox);
 
-
+//Links clicked cells to array values
     const getNumberFromClick = function (event){
-    console.log(gameboard.currentPlayer);
         
-
-
-
     if(event.target.id==="square-0"){gameboard.squareClicked=0;}
     else if(event.target.id==="square-1"){gameboard.squareClicked=1;}
     else if(event.target.id==="square-2"){gameboard.squareClicked=2;}
@@ -235,8 +179,6 @@ let displayController = function(){
     else if(event.target.id==="square-8"){gameboard.squareClicked=8;}
 
     
-
-
     if (gameboard.board[gameboard.squareClicked]==='X' || gameboard.board[gameboard.squareClicked]==='0')
         {alert('That square is taken, please select another')
     gameboard.currentPlayer = gameboard.currentPlayer===firstPlayerObject.name? gameboard.currentPlayer=secondPlayerObject.name: gameboard.currentPlayer=firstPlayerObject.name;
@@ -246,24 +188,18 @@ let displayController = function(){
 
     else if (gameboard.currentPlayer===firstPlayerObject.name) {gameboard.board[gameboard.squareClicked]='X'}
     else {gameboard.board[gameboard.squareClicked]='0'}
-square0.innerText=gameboard.board[0];
-square1.innerText=gameboard.board[1];
-square2.innerText=gameboard.board[2];
-square3.innerText=gameboard.board[3];
-square4.innerText=gameboard.board[4];
-square5.innerText=gameboard.board[5];
-square6.innerText=gameboard.board[6];
-square7.innerText=gameboard.board[7];
-square8.innerText=gameboard.board[8];            
+        square0.innerText=gameboard.board[0];
+        square1.innerText=gameboard.board[1];
+        square2.innerText=gameboard.board[2];
+        square3.innerText=gameboard.board[3];
+        square4.innerText=gameboard.board[4];
+        square5.innerText=gameboard.board[5];
+        square6.innerText=gameboard.board[6];
+        square7.innerText=gameboard.board[7];
+        square8.innerText=gameboard.board[8];            
 
 
-    console.log(gameboard.currentPlayer);
     gameboard.currentPlayer = gameboard.currentPlayer===firstPlayerObject.name? gameboard.currentPlayer=secondPlayerObject.name: gameboard.currentPlayer=firstPlayerObject.name;
-
-
-
-    console.log(gameboard.squareClicked);
-    console.log({gameboard});
 
     playGame();        }
     
@@ -272,17 +208,11 @@ square8.innerText=gameboard.board[8];
         resetButton.addEventListener('click', gameboard.resetBoardAndGame);
 
 
-
-
-
-
-  
     return {playerNamesContainer, boardPlusReset, firstPlayerObject, secondPlayerObject, playerOneName, playerTwoName, squareClicked, messageBox, square0, square1, square2, square3, square4, square5, square6, square7, square8};  
 
 
         }();
 
 
-console.log({displayController});
 
 
